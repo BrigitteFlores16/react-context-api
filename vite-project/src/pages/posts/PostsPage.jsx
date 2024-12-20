7a27aba3a05d9f5a54d6392dae74d1d5a1c70845
BrigitteFlores16 <macalupufloresyiyibrigitte@gmail.com>
2024-12-20 16:25:54 +0100

    first commit

---
+++ vite-project/src/pages/posts/PostsPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PostsList from "../PostsList";
+
+export default function PostsPage() {
+  return (
+    <div>
+      <h1>Lista dei Post</h1>
+      <PostsList />
+    </div>
+  );
+}
